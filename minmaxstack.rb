6f0b662916c20e01ecb25941a467d544a88e5ebe
n <nevindnl@gmail.com>
2016-10-24 00:09:47 -0700

    minmaxstackqueue

---
+++ minmaxstack.rb
@@ -0,0 +1,51 @@
+class MyStack
+  attr_reader :store
+
+  def initialize
+    @store = []
+  end
+
+  def max
+    @store.empty? ? nil : @store[-1][2]
+  end
+
+  def min
+    @store.empty? ? nil : @store[-1][1]
+  end
+
+  def push el
+    @max = el if empty? || el > @max
+    @min = el if empty? || el < @min
+
+    if @store.empty?
+      @store << [el, el, el]
+    else
+      @store << [el, @min, @max]
+    end
+
+    el
+  end
+
+  def pop
+    el = @store.pop
+    if empty?
+      @min, @max = nil, nil
+    else
+      _, @min, @max = @store.last
+    end
+
+    el
+  end
+
+  def peek
+    @store.last
+  end
+
+  def size
+    @store.length
+  end
+
+  def empty?
+    @store.empty?
+  end
+end
---
+++ minmaxstackqueue.rb
@@ -0,0 +1,41 @@
+require_relative 'minmaxstack'
+require_relative 'queue'
+
+class MinMaxStackQueue
+  def initialize
+    @store1 = MyStack.new
+    @store2 = MyStack.new
+  end
+
+  def enqueue el
+    @store1.push el
+  end
+
+  def dequeue
+    @store1.size.times{ @store2.push(@store1.pop[0]) }
+    @store2.pop
+    @store2.size.times{ @store1.push(@store2.pop[0]) }
+  end
+
+  def peek
+    @store1.size.times{ @store2.push(@store1.pop[0]) }
+    @store2.peek[0]
+    @store2.size.times{ @store1.push(@store2.pop[0]) }
+  end
+
+  def max
+    @store1.max
+  end
+
+  def min
+    @store1.min
+  end
+
+  def size
+    @store1.size
+  end
+
+  def empty?
+    @store1.empty?
+  end
+end
